e199a3baf2d9e1b3871c741ded77a13754ea4c3c
Daniel Siepmann <coding@daniel-siepmann.de>
2019-02-12 22:27:49 +0100

    TASK: Provide task to only build assets
    This tasks excludes build_sphinx, in order to allow any frontend dev to
    compile assets with as less as possible dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,6 +133,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-open');
 
   grunt.registerTask('fonts', ['clean:fonts','copy:fonts']);
+  grunt.registerTask('assets', ['exec:bower_update','clean:build','sass:build']);
   grunt.registerTask('default', ['exec:bower_update','clean:build','sass:dev','exec:build_sphinx','connect','open','watch']);
   grunt.registerTask('build', ['exec:bower_update','clean:build','sass:build','exec:build_sphinx']);
 }
