0cf7ddf20635c3e9501a476aa23295f9ceb43068
Benjamin Kott <benjamin.kott@outlook.com>
2019-12-05 10:13:38 +0100

    [TASK] Add fontawesome

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,6 +62,7 @@ module.exports = function (grunt) {
         files: {
           't3SphinxThemeRtd/static/css/theme.css': 'sass/theme.scss',
           't3SphinxThemeRtd/static/css/webfonts.css': 'sass/webfonts.scss',
+          't3SphinxThemeRtd/static/css/fontawesome.css': 'sass/fontawesome.scss',
         }
       }
     },
