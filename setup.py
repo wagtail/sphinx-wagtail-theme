9a247b0c80c83a44e1fdd73283941ef9f62bb8c0
Benjamin Kott <benjamin.kott@outlook.com>
2020-01-23 11:47:02 +0100

    [TASK] Cleanup

--- setup.py
+++ setup.py
@@ -14,31 +14,35 @@ setup(
     license='MIT',
     author='Martin Bless',
     author_email='martin.bless@mbless.de',
-    description='Docs.TYPO3.Org theme for Sphinx, starting 2015.',
-    long_description=open('README.rst').read(),
+    description='Sphinx TYPO3 theme for docs.typo3.org, starting 2015.',
+    long_description=open('README.rst', encoding='utf-8').read(),
     zip_safe=False,
     packages=['sphinx_typo3_theme'],
     package_data={'sphinx_typo3_theme': ['*.*']},
     include_package_data=True,
-    install_requires=open('requirements.txt').read().splitlines(),
+    entry_points = {
+        'sphinx.html_themes': [
+            'sphinx_typo3_theme = sphinx_typo3_theme',
+        ]
+    },
+    install_requires=[
+       'sphinx'
+    ],
+    tests_require=[
+        'pytest',
+    ],
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+        'Framework :: Sphinx',
+        'Framework :: Sphinx :: Theme',
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: End Users/Desktop',
-        'Framework :: Sphinx :: Extension',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent',
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
