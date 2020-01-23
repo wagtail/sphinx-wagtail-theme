c38c4133d7299937ebf3ef5dd7a777a7c28e4be9
Benjamin Kott <benjamin.kott@outlook.com>
2020-01-23 11:06:28 +0100

    [TASK] Minor cleanups

--- my-sphinx-build.sh
+++ my-sphinx-build.sh
@@ -6,6 +6,6 @@
 sphinx-build $1 $2
 
 if ((1)); then
-	mv $2/_static $2/__static
-	ln -s $(pwd)/sphinx_typo3_theme/static $2/_static
+    mv $2/_static $2/__static
+    ln -s $(pwd)/sphinx_typo3_theme/static $2/_static
 fi
