0cabf251a8434c26186b203277d29618386f25f8
Martin Bless <martin.bless@mbless.de>
2020-11-28 21:54:27 +0100

    ...

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
       options: {
         implementation: sass,
         outputStyle: 'compressed',
-        sourceMap: false
+        sourceMap: true
       },
       build: {
         files: {
