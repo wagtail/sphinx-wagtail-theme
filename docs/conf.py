5176ee511189102a13618a849f45bf64df63fe80
Coen van der Kamp <coen@fourdigits.nl>
2021-03-10 22:43:51 +0100

    Release process

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,25 @@
-#!/usr/bin/env python
-#
 import os
 import sys
+
+import sphinx_wagtail_theme
+
 sys.path.insert(0, os.path.abspath('..'))
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx_wagtail_theme',
     ]
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'Sphinx Wagtail theme'
-copyright = "2020, Wagtail documentation team"
-author = "Martin Bless"
+copyright = "2020, Wagtail contributors"
+author = "Wagtail"
 # The short X.Y version.
-version = '0.0'
+version = sphinx_wagtail_theme.__version__
 # The full version, including alpha/beta/rc tags.
-release = '0.0.0'
+release = sphinx_wagtail_theme.__version__
 language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
@@ -26,7 +28,7 @@ todo_include_todos = False
 today_fmt = '%Y-%m-%d %H:%M'
 
 # -- Options for HTML output -------------------------------------------
-html_theme = 'alabaster'
+html_theme = 'sphinx_wagtail_theme'
 html_static_path = []
 # -- Options for HTMLHelp output ---------------------------------------
 htmlhelp_basename = 'sphinx_wagtail_themedoc'
@@ -52,18 +54,3 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-# -- what we are doing -------------------------------------------------
-try:
-    import sphinx_wagtail_theme
-    is_imported = True
-except:
-    is_imported = False
-if is_imported:
-    version = sphinx_wagtail_theme.__version__
-    release = sphinx_wagtail_theme.__version__
-    html_theme = 'sphinx_wagtail_theme'
-
-
-
-
-
