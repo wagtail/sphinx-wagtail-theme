d6f1b70762e048baa5ef60026f656c4714aa6080
LB Johnston <lb.johnston@virginaustralia.com>
2022-04-07 08:28:31 +1000

    disable pygments theme
    - see sphinx_wagtail_theme/theme.conf (already disabled there)
    - this project has its own pygments styling to suit the code block colours
    - see https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-pygments_style
    - fixes #147

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ version = sphinx_wagtail_theme.__version__
 release = sphinx_wagtail_theme.__version__
 language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'requirements.txt']
-pygments_style = 'sphinx'
+pygments_style = None
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 today_fmt = '%Y-%m-%d %H:%M'
