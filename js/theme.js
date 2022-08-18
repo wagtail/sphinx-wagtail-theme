8a3cec08b64b1c03d2616629db900e0a2200b0a5
Vince Salvino <salvino@coderedcorp.com>
2022-08-18 13:37:31 -0400

    Add Dark mode 🧛
    - show the page buttons on small devices also

--- js/theme.js
+++ js/theme.js
@@ -8,6 +8,61 @@ if (typeof window.T3Docs === "undefined") {
   window.T3Docs = {};
 }
 
+/**
+ * Set and store dark/light mode.
+ *
+ * Mode will be set to be one of "light" or "dark", by default the resolved
+ * theme mode will be flipped (e.g. light to dark).
+ *
+ * @param {Event} event - is for usage with event listeners, and indicates the user has manually clicked a button
+ * @param {object?} options
+ * @param {boolean?} options.isInitial - if true the current value will be resolved & set NOT toggled
+ */
+function updateDarkMode(event, { isInitial = false } = {}) {
+  const DARK = "dark";
+  const LIGHT = "light";
+  const STORAGE_KEY = "wagtail-theme";
+
+  let currentMode;
+  let applyMode;
+  const savedThemeMode = localStorage.getItem(STORAGE_KEY);
+
+  // find the current mode from existing storage or browser preference
+  if (savedThemeMode) {
+    // note - do not assume correct format of local storage
+    currentMode = savedThemeMode === LIGHT ? LIGHT : DARK;
+  } else {
+    // fall back on browser media for first toggle
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme:dark)"
+    ).matches;
+
+    currentMode = prefersDarkMode ? DARK : LIGHT;
+  }
+
+  // if running initially - do not 'flip' - instead apply current mode
+  if (isInitial) {
+    applyMode = currentMode === DARK ? DARK : LIGHT;
+  } else {
+    applyMode = currentMode === DARK ? LIGHT : DARK;
+  }
+
+  // set applied mode to the DOM
+  document.body.className = applyMode === DARK ? "theme-dark" : "";
+
+  // only store value if already stored OR was triggered by an actual click
+  if (savedThemeMode || event) {
+    localStorage.setItem(STORAGE_KEY, applyMode);
+  }
+}
+
+/**
+ * Apply user's saved light/dark preference, or fall back to browser setting.
+ * Load light/dark preference before DOMContentLoaded so the user does not see a
+ * flash during theme initialisation.
+ */
+updateDarkMode(null, { isInitial: true });
+
 // Inject collapsible menu
 function toggleCurrent(event) {
   event.preventDefault();
@@ -51,8 +106,13 @@ function makeTablesResponsive() {
 
 makeTablesResponsive();
 
-// Search
 document.addEventListener("DOMContentLoaded", () => {
+  // Wire up light/dark mode button.
+  document
+    .getElementById("wagtail-theme")
+    .addEventListener("click", updateDarkMode);
+
+  // Search.
   var searchform = document.getElementById("search-form");
   var searchinput = document.getElementById("searchinput");
   if (searchform && searchinput) {
